69dafa1a4eb5fcc363bc6955d888ee63e46323e3
Hoang Ngoc <hoangngoc3691@gmail.com>
2026-05-03 17:10:07 +0700

    feat: add framework for 2 dataset

---
+++ src/dataset/market1051.py
@@ -0,0 +1,22 @@
+import numpy as np
+from base import Object
+
+class Market1501(Object):
+
+    def __init__(self):
+        super().__init__()
+
+    def load(path):
+        return super().load()
+    
+    def save(path):
+        return super().save()
+    
+    def clone():
+        pass
+
+    def info():
+        return
+    
+    def accept():
+        return
---
+++ src/dataset/msmt17.py
@@ -0,0 +1,21 @@
+import numpy as np
+from base import Object
+
+class MSMT17(Object):
+    def __init__(self):
+        super().__init__()
+
+    def load(path):
+        return super().load()
+    
+    def save(path):
+        return super().save()
+    
+    def clone():
+        return
+    
+    def info():
+        return
+    
+    def accept():
+        return
